044337b81329f50555617c345192a5a3e3f48082
urethiuui1 <>
2022-03-02 09:38:14 +0100

    delete module-info.java

--- module-info.java
+++
@@ -1,8 +0,0 @@
-module checker {
-	requires transitive javafx.graphics;
-	requires transitive javafx.controls;
-	requires javafx.fxml;
-	requires javafx.base;
-	exports checker to javafx.graphics;
-	opens checker;
-}
